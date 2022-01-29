721a7136e5970f91e2dfbf1af1f4edf89c7d559b
zpdl95 <59528534+zpdl95@users.noreply.github.com>
2022-01-29 18:06:32 +0900

    #0.6 Interfaces on Typescript

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,19 @@
-const sayHi = (
-  name: string /* ← 함수의 인자 값에 type을 설정 */,
-  age: number,
-  gender: string
-): string /* ← 함수의 return값에 type을 설정 */ => {
-  return `Hello ${name}, you are ${age}, you are a ${gender}`;
+/* ts에서만 동작하는 인터페이스, 오브젝트의 타입을 명명한다, 자동완성기능도 추가해줌 */
+interface Human {
+  name: string;
+  age: number;
+  gender: string;
+}
+
+const person = {
+  name: "Jaewon",
+  age: 24,
+  gender: "male",
 };
 
-console.log(sayHi("Jaewon", 24, "male"));
+const sayHi = (person: Human): string /* ← 함수의 return값에 type을 설정 */ => {
+  return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}`;
+};
+
+console.log(sayHi(person));
 export {};
