2b47de6b191e57e192825b7182479f8312713f80
zpdl95 <59528534+zpdl95@users.noreply.github.com>
2022-01-29 19:07:13 +0900

    #0.7 Classes on Typescript part One

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,21 @@
-/* ts에서만 동작하는 인터페이스, 오브젝트의 타입을 명명한다, 자동완성기능도 추가해줌 */
-interface Human {
-  name: string;
-  age: number;
-  gender: string;
+/* 일반 js에서 type을 사용하고 싶으면 클래스를 사용해서 type지정하면됨 */
+class Human {
+  public name: string; /* public = 클래스 외부에서 접근가능 */
+  public age: number;
+  public gender: string;
+  /* constructor = 생성자 method. 클래스가 실행할 때마다 호출 */
+  constructor(name: string, age: number, gender: string) {
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+  }
 }
 
-const person = {
-  name: "Jaewon",
-  age: 24,
-  gender: "male",
-};
+const lynn = new Human("Lynn", 25, "female");
 
 const sayHi = (person: Human): string /* ← 함수의 return값에 type을 설정 */ => {
   return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}`;
 };
 
-console.log(sayHi(person));
+console.log(sayHi(lynn));
 export {};
